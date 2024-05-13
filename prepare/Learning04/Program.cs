82e8be8a54a977f2616f85db2626efe75524c193
Marce <lmvt.07@gmail.com>
2024-05-13 15:38:42 -0600

    Assignments

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Roberto Rodriguez", "Multiplication");
+        string summaryAssignment = assignment.GetSummary();
+        Console.WriteLine(summaryAssignment);
+
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        string mathAssignmentSummary = mathAssignment.GetSummary();
+        string homeWorkListMathAssignment = mathAssignment.GetHomeworkList();
+        Console.WriteLine(homeWorkListMathAssignment);
+        Console.WriteLine(mathAssignmentSummary);
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        string getStudentInformation = writingAssignment.GetSummary();
+        string getTitleInformation = writingAssignment.GetWritingInformation();
+        Console.WriteLine(getTitleInformation);
+        Console.WriteLine(getStudentInformation);
     }
 }
