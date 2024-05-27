f63fda1452f113f6a7ffef5fa82369cbabb862a6
Marce <lmvt.07@gmail.com>
2024-05-27 08:59:42 -0600

    eternal-quest

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,30 @@
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected string _points;
+
+    public Goal (string name, string description, string points) {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+    public abstract bool IsComplete();
+
+    public string GetShortName() {
+        return _shortName;
+    }
+
+    public virtual string GetDetailsString() {
+        return $"[{(IsComplete() ? "X" : " ")}] {_shortName} ({_description})";
+    }
+
+    public virtual int GetPoints() {
+        return int.Parse(_points);
+    }
+
+    public abstract string GetStringRepresentation();
+
+}
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        GoalManager goalManager = new GoalManager();
+        goalManager.Start();
+
+
+    //    bool keepRunning = true;
+
+    //    Console.WriteLine("You have 0 points.");
+
+    //    while(keepRunning == true)
+    //    {
+    //      Console.WriteLine("Menu Options:");
+    //      Console.WriteLine("1. Create New Goal");
+    //      Console.WriteLine("2. List Goal");
+    //      Console.WriteLine("3. Save Goal");
+    //      Console.WriteLine("4. Load Goal");
+    //      Console.WriteLine("5. Record Event");
+    //      Console.WriteLine("6. Quit");
+    //      Console.WriteLine("Select a choice from the menu: ");
+
+    //      string mainOptionString = Console.ReadLine();
+    //      int mainOptionInt = int.Parse(mainOptionString);
+
+    //      if (mainOptionInt == 1)
+    //      {
+    //         Console.WriteLine("The type of Goals are:");
+    //         Console.ReadLine();
+            
+    //      }
+    //      else if (mainOptionInt == 2)
+    //      {
+    //         Console.ReadLine();
+
+    //      }
+    //      else if (mainOptionInt == 3)
+    //      {
+    //         Console.ReadLine();
+
+    //      }
+    //      else if (mainOptionInt == 4)
+    //      {
+    //         Console.ReadLine();
+
+    //      }
+    //      else if (mainOptionInt == 5)
+    //      {
+    //         Console.ReadLine();
+
+    //      }
+    //      else if (mainOptionInt == 6)
+    //      {
+    //         keepRunning = false;
+    //      }
+    //     else
+    //     {
+    //             Console.WriteLine("Please enter a valid option");
+    //     }
+
+
+
+    //    }
     }
 }
