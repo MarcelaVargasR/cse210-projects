0419d25c6c4ba9e528fb5d967572039c302947f7
Marce <lmvt.07@gmail.com>
2024-06-03 08:11:16 -0600

    Excersice

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activitiesList = new List<Activity>();
+
+        Running running = new Running(45, 5);
+        activitiesList.Add(running);
+        Cycling cycling = new Cycling(70, 20);
+        activitiesList.Add(cycling);
+        Swimming swimming = new Swimming(30, 30);
+        activitiesList.Add(swimming);
+
+
+        foreach (Activity activity in activitiesList)
+        {
+            activity.DisplaySummary();
+        }
     }
 }
